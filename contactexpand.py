b054cbc893f8028f6d6012a304f74bea8c035c65
Denis Borchev <dborchev@gmail.com>
2017-11-04 10:58:29 +0300

    do code clean-up

--- contactexpand.py
+++ contactexpand.py
@@ -4,36 +4,30 @@
 
 import csv
 
-
-
-def genFull():
-    # generates full list for all users
+def LoadContacts():
     contactsReader = csv.DictReader(open(r'contacts.csv'))
     userlist = list()
     for row in contactsReader:
         userlist.append((row['Contact ID'] , row['Contact Domain'],row['Nickname'],row['Group Name'] ))
-    with open(r'output.csv', 'w', newline='') as csvfile:
-        resultwriter = csv.writer(csvfile, dialect = 'excel')
-        for row in userlist:
-            print(row)
+    return userlist
+
+def genFull(resultwriter=None, userlist=None):
+    # generates full list for all users
+    for row in userlist:
+            #
             for inrow in userlist:
                 if not inrow[0] == row[0]: resultwriter.writerow([row[0],row[1],inrow[0],row[1],row[2],row[3]])
                 #
-def genNewUser(name=list()):
+def genNewUser(name=list(),resultwriter=None, userlist=None):
     # generates lists for selected users and adds them to everybody else's individual contact lists
-    if name == list() : return genFull()
-    contactsReader = csv.DictReader(open(r'contacts.csv'))
-    userlist = list()
-    for row in contactsReader:
-        userlist.append((row['Contact ID'] , row['Contact Domain'],row['Nickname'],row['Group Name'] ))
-    with open(r'output.csv', 'w', newline='') as csvfile:
-        resultwriter = csv.writer(csvfile, dialect = 'excel')
-        for row in userlist:
-            print(row)
+    if name == list() : return genFull(resultwriter, userlist)
+    #
+     for row in userlist:
+            #
             for inrow in userlist:
                 if not inrow[0] == row[0]: 
                     if inrow[0] in name: resultwriter.writerow([row[0],row[1],inrow[0],inrow[1],inrow[2],inrow[3]])
-        for row in userlist:
+            #
             if row[0] in name: 
                 for inrow in userlist:
                     if not inrow[0] == row[0]: resultwriter.writerow([row[0],row[1],inrow[0],inrow[1],inrow[2],inrow[3]])
@@ -45,5 +39,8 @@ if __name__ == '__main__':
     'user3',] # list of users for whom to generate contacts
               # i.e. newcoming employees
               # if empty - generate full list for all users
-    genNewUser(name=namelist)
+    userinput = LoadContacts()
+    with open(r'output.csv', 'w', newline='') as csvfile:
+        result = csv.writer(csvfile, dialect = 'excel')
+        genNewUser(name=namelist, resultwriter=result, userlist=userinput)
     # EOF
